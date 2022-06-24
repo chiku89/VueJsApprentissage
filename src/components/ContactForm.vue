9cb5d151d6870c039ac13daae29553d51d56f4d4
Gauthier FONTAINE <gauthier.fontaine@gmail.com>
2022-06-24 18:10:14 +0200

    Création du formulaire d'ajout de contacts + initialisation de firebase

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <form @submit.prevent="createContact">
+            <input type="text" placeholder="Prénom" v-model="contact.firstName"><br>
+            <input type="text" placeholder="Nom" v-model="contact.lastName"><br>
+            <input type="text" placeholder="Email" v-model="contact.mail"><br>
+            <input type="text" placeholder="Téléphone" v-model="contact.phone"><br>
+            <button type="submit">Créer</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    import db from '../shared/db';
+    export default {
+        data() {
+            return {
+                contact: {
+                firstName: "",
+                lastName: "",
+                email: "",
+                phone: ""
+                }
+            };
+        },
+        methods: {
+           createContact() {
+            db.create(this.contact)
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+           } 
+        },
+    };
+</script>
+
+<style scoped>
+</style>
